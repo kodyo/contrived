277027b2dbd49d76c719fdbd1f1aa5cb643b358d
Kody Dickerson <kody.dickerson@shorepointinc.com>
2024-01-11 12:35:50 -0800

    Comments for serializer

--- DemoProject/helpers/serializer.py
+++ DemoProject/helpers/serializer.py
@@ -6,8 +6,15 @@ class GenericSerializer():
         self.outfile = output_file
 
     def serialize(self, myobject: object):
+        '''
+        Return JSON string dumped from the object contents
+        '''
         return json.dumps(myobject.__dict__, indent=2)
     
     def serialize_to_file(self, myobject: object):
+        '''
+        Write dumped JSON string to the file specified when
+        GenericSerializer class is initialized.
+        '''
         with open(self.outfile, 'w') as f:
             f.write(json.dumps(myobject.__dict__, indent=2))
